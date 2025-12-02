20c94160d19307dc3d4d9bcaca3af3a2a41862af
Afsaar ansari <afsaaransari786@outlook.com>
2025-12-02 21:15:12 +0530

    w

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,35 @@ import app from "./src/app.js";
 
 connectDB();
 
+let isConnected = false
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    isConnected = true;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+app.use((re,res,next)=>{
+  if(!isConnected) {
+    connectToMongoDB();
+  }
+  next();
+})
+
 app.use("/",(req,res)=>{
   res.send("hellow bro")
 })
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  /// callback
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   /// callback
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
-
+export default app
