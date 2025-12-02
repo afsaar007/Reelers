2d5307e11ebd6dac06e01d4f4b9b5da6b71d7378
Afsaar ansari <afsaaransari786@outlook.com>
2025-12-02 21:26:00 +0530

    first update

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,19 @@
 /// start
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express"
 import connectDB from "./src/db/db.js";
 import app from "./src/app.js";
 
 
 connectDB();
 
-let isConnected = false
-async function connectToMongoDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
-    isConnected = true;
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-
-app.use((re,res,next)=>{
-  if(!isConnected) {
-    connectToMongoDB();
-  }
-  next();
-})
-
 app.use("/",(req,res)=>{
   res.send("hellow bro")
 })
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   /// callback
-//   console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  /// callback
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app
