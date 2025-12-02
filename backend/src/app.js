7a4d293fc7e997bf96542b88ee2fa2a81b51556c
Afsaar ansari <afsaaransari786@outlook.com>
2025-12-02 12:52:40 +0530

    update!

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import foodPartnerRoutes from './routes/food-partner.routes.js'
 
 const app = express();
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"reelers-vgnf-nudk1iqfc-afsaar-ansaris-projects-4b38e79e.vercel.app",
   credentials:true
 }));
 
