9b81b6bd23ee41e36bca7378cdd64225ad31e64d
Afsaar ansari <afsaaransari786@outlook.com>
2025-12-02 20:07:06 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,9 @@ dotenv.config();
 import express from "express"
 import connectDB from "./src/db/db.js";
 import app from "./src/app.js";
-import path from "path"; ///deploy
+
 
 connectDB();
-
-const _dirname = path.resolve();
-
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
-
- app.use((req,res) =>{
-  res.sendFile(path.resolve(_dirname,"frontend", "dist", "index.html"));
- })
 
 
 const PORT = process.env.PORT || 3000;
