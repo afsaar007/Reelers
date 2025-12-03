755a971b852c9f5a46d0468e14e173195e5b7137
Afsaar ansari <afsaaransari786@outlook.com>
2025-12-03 07:59:53 +0530

    jh

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ import foodPartnerRoutes from './routes/food-partner.routes.js'
 
 const app = express();
 app.use(cors({
-  origin:["http://localhost:5173", "https://reelers-9.onrender.com"],
+  origin:"https://reelers-9.onrender.com",
   credentials:true
 }));
 
